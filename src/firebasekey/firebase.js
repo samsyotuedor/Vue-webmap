b531b2ea2d9dfd4689f9abe1ea691d9980b8c098
samsyotuedor <samsyotuedor40@gmail.com>
2021-02-19 02:33:13 +0100

    edit file

--- src/firebasekey/firebase.js
+++ src/firebasekey/firebase.js
@@ -3,7 +3,14 @@ import 'firebase/analytics'
 import 'firebase/auth'
 import 'firebase/firestore'
 // Your web app's Firebase configuration
-
+const firebaseConfig = {
+  authDomain: 'webmap-18f07.firebaseapp.com',
+  databaseURL: 'https://webmap-18f07.firebaseio.com',
+  projectId: 'webmap-18f07',
+  storageBucket: 'webmap-18f07.appspot.com',
+  messagingSenderId: '1069585918532',
+  appId: '1:1069585918532:web:01554544731df2e7b16a11'
+}
 // Initialize Firebaseeport
 export default () => {
   firebase.initializeApp(firebaseConfig)
